0894a0d39bffc7340764ff2a6da3a4d45f007ac8
jydnbach <jaydenkim95@gmail.com>
2024-07-27 13:32:25 +0900

    expressing actions as type union

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 export enum ActionTypes {
   fetchTodos,
   deletetodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
